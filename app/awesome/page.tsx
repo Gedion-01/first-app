35d1aa5443a9102f4de83fc90d184006168edfa2
gedion <gedion@domain.com>
2024-05-12 09:27:07 +0300

    update

--- app/awesome/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AwesomePage() {
-  console.log('AwesomePage');
-  
-  return (
-    <main>
-      <h1>next js, Awesome page</h1>
-    </main>
-  )
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <p><Link href="/about">About us</Link></p>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Share Meal</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
     </main>
   );
 }
