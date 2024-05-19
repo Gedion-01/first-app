67a8cde42088da171b08aa2a80d9fde766641b1b
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-05-19 10:50:46 +0300

    update

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import { Metadata } from "next";
 import { Suspense } from "react";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
+export const metadata: Metadata = {
+  title: "All Meals",
+  description: "Browse the delecious meals shared by our community.",
+};
+
 async function Meals() {
   const meals = await getMeals();
-  return <MealsGrid meals={meals} />
+  return <MealsGrid meals={meals} />;
 }
 
 export default function MealsPage() {
@@ -25,7 +31,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
           <Meals />
         </Suspense>
       </main>
